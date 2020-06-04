f791e985ad30aae31e06d40424ce7b9a79710264
fearless-command-2358 <iuriseara@gmail.com>
2020-06-04 19:59:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,12 +6,16 @@ def nyc_pigeon_organizer(data)
     value.each do |inner_key, names|
       names.each do |name|
 
+        #check to see if pigeon name already exists.  If not, create an empty hash
         if !final_array[name]
           final_array[name] = {}
         end
+
+          #check to make sure pigeon_list[:name][:color] exists
           if !final_array[name][key]
             !final_array[name][key] = []
           end
+          #push color value to new hash and converts to a string.
           final_array[name][key].push(inner_key.to_s)
       end
     end
